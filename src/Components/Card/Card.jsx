329698bba0d995093d6685f436bf07cba63cbaf9
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-17 14:46:25 +0200

    iniziata personalizzazione del messaggio da inviare in chat

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,12 +5,14 @@ export default function Card({card}) {
     const botToken = `${process.env.REACT_APP_BOT_TOKEN}`;
     const chatId = `${process.env.REACT_APP_CHAT_ID}`;
 
-    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=<a href="${msj}"></a>`);
+    const message = `${msj.id}\n[inline URL](${msj.images.small})`
+
+    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}`);
 
   };
   return (
     <div className='card'
-      onClick={() => {sendMessage(card.images.small)}}>
+      onClick={() => {sendMessage(card)}}>
         <h3 id='cardId'>{card.id}</h3>
         <img src={card.images.small} className='imageCard' alt=''></img>
     </div>
