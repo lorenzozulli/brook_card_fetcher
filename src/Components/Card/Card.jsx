7244bf06436cdf4b9169714a062de4c18824dc12
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-17 14:34:39 +0200

    prova

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,12 +5,12 @@ export default function Card({card}) {
     const botToken = `${process.env.REACT_APP_BOT_TOKEN}`;
     const chatId = `${process.env.REACT_APP_CHAT_ID}`;
 
-    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${msj}`);
+    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=<a href="${msj}"></a>`);
 
   };
   return (
     <div className='card'
-      onClick={() => {sendMessage(card.images.small)}}>
+      onClick={() => {sendMessage(card.id)}}>
         <h3 id='cardId'>{card.id}</h3>
         <img src={card.images.small} className='imageCard' alt=''></img>
     </div>
