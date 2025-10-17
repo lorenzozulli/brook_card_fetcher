f4b5199478a43ed2b8f666302e05f81427a01567
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-17 15:39:10 +0200

    aggiunto chat id del gruppo, devo vedere come si mette quello custom

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css';
 export default function Card({card}) {
   const sendMessage = async (msj) => {
     const botToken = `${process.env.REACT_APP_BOT_TOKEN}`;
-    const chatId = message.chat.id;
+    const chatId = `${process.env.REACT_APP_CHAT_ID}`;
 
     const message = `Id: ${msj.id}
     Name: ${msj.name}
