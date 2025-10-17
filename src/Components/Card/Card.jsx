51c84cfc6e4098e26bfa7f82bd5c274ca3033cd1
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-17 17:59:47 +0200

    rimosso codice

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,8 +6,6 @@ export default function Card({card}) {
     const botToken = `${process.env.REACT_APP_BOT_TOKEN}`;
     const chatId = `${process.env.REACT_APP_CHAT_ID}`;
 
-    console.log(Telegram.WebApp.initDataUnsafe)
-
     const message = `Id: ${msj.id}
     Name: ${msj.name}
     Image: ${msj.images.small}`
