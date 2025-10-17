4c7df13a4fc4a1325c3d41d8f66d7d5dfdbb42e4
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-17 14:14:40 +0200

    aggiunto sendMessage

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,16 @@
 import './Card.css';
 
 export default function Card({card}) {
+  const sendMessage = async (msj) => {
+    const botToken = `${process.env.REACT_APP_BOT_TOKEN}`;
+    const chatId = `${process.env.REACT_APP_CHAT_ID}`;
+
+    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${msj}`);
+
+  };
   return (
-    <div className='card'>
+    <div className='card'
+      onClick={() => {sendMessage(card.id)}}>
         <h3 id='cardId'>{card.id}</h3>
         <img src={card.images.small} className='imageCard' alt=''></img>
     </div>
