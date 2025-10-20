908a4ec845ca88baf35362e31a52fd45016d1067
lorenzozulli <zullilorenzo.24@gmail.com>
2025-10-20 14:02:31 +0200

    vediamo se sto sbagliando io a lanciato

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
 
       WebApp.ready(); 
 
-      const initData = WebApp.WebAppChat;
+      const initData = WebApp.initDataUnsafe;
 
-      if (initData.id) {
-        setInitDataValue(initData.id);
+      if (initData.chat.id) {
+        setInitDataValue(initData.chat.id);
       } else {
         console.error("Oggetto 'chat' non trovato. Probabilmente la TWA è stata avviata da una chat privata senza contesto o non ha i permessi.");
       }
